3136c6a51cc90f617ee41d7f52a23877e1e82dc8
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-03-17 22:19:15 +0100

    changing files

---
+++ backend/nodejs/webbserver.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.json());
+
+//connect to mongoose
+//require route
+
+app.listen(3001, function() {
+    
+})
